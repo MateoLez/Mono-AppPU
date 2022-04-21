6cd474f8c3de5590bbe59862cd65d52d6f3f2d1b
Mateo Gonzalez <70272782+MateoLez@users.noreply.github.com>
2022-04-21 15:01:59 -0500

    Laravel JetStream, AdminLTE3

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,23 @@
+
+
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+<x-app-layout>
+
+    <div class="py-12">
+        <h1>Hi</h1>
+    </div>
+</x-app-layout>
+
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
